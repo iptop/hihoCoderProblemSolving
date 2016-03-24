fe3b33759eb552639c41c514fe172d46264bcaca
120267583@qq.com <120267583@qq.com>
2016-03-24 10:11:13 +0800

    Signed-off-by: 120267583@qq.com <120267583@qq.com>

---
+++ 1000/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <map> 
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int a,b;
+	while(cin>>a>>b)
+	{
+		cout<<a+b<<endl;
+	}
+	return 0;
+}
