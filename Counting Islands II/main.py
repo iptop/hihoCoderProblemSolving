6e1d4dc541e5d39c97691e9528961c9de378fa24
120267583@qq.com <120267583@qq.com>
2016-07-30 14:25:02 +0800

    Signed-off-by: 120267583@qq.com <120267583@qq.com>

--- Counting Islands II/main.py
+++ Counting Islands II/main.py
@@ -1,41 +1,32 @@
-islandSet={} #全部集合
-
+islandSet={}
 n=int(raw_input())
-
-def countIsland(): #计算全部集合总共有几个根节点
-    RootSet={}
-    for k in islandSet:
-        root= getRoot(k[0],k[1])
-        RootSet[root]=1
-    print(len(RootSet) )
-
-def getRoot(x,y): #获得集合的根节点
+num=0
+def getRoot(x,y):
     parent=islandSet[(x,y)]
     if(parent==(x,y)):
         return (x,y)
     else:
-        return getRoot(parent[0],parent[1])
-
-
-def test(x,y,nx,ny): #判断是否有相邻的集合
+        root=getRoot(parent[0],parent[1])
+        islandSet[(x,y)]=root
+        return root
+def test(x,y,nx,ny):
+    global num
     if islandSet.has_key((x,y)):
         root = getRoot(x,y)
-        islandSet[root]=(nx,ny)
-
-
-def addPoint(x,y): #添加集合
+        if(root!=(nx,ny)):
+            islandSet[root]=(nx,ny)
+            num-=1
+def addPoint(x,y):
+    global num
     islandSet[(x,y)]=(x,y)
+    num=num+1
     if x>0:test(x-1,y,x,y)
     if(y>0):test(x,y-1,x,y)
     if(x<999):test(x+1,y,x,y)
     if(y<999):test(x,y+1,x,y)
-
 for i in range(0,n):
     line =raw_input().split(" ")
     x=int(line[0])
     y=int(line[1])
     addPoint(x,y)
-    countIsland()
-
-
-
+    print(num)
